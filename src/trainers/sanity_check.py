05f99d9e3f5ebdc15732183883120737683d4d80
Elsa Riachi <elsa.riachi@mail.mcgill.ca>
2019-03-07 17:35:45 -0500

    Fix in greedy decoding, added id2lang

--- src/trainers/sanity_check.py
+++ src/trainers/sanity_check.py
@@ -35,6 +35,7 @@ class LanguageModeling(Trainer):
     
     def greedy_decoding(self, sent):
 
+        assert(sent.shape[0] == 1)
         lang = 0
         src_mask = self.get_src_mask(sent)
         latent_code = self.transformer.encode(input_seq=sent,
@@ -53,18 +54,19 @@ class LanguageModeling(Trainer):
             tgt_mask = np.ones((1, self.max_len))
             tgt_mask = torch.from_numpy(tgt_mask)
             tgt_mask = tgt_mask.masked_fill_(prev_output == self.pad_index, 0)
-            dec_logits = self.transformer.decode(prev_output=prev_output,
+            dec_logits = self.transformer.decode(prev_output=prev_output[:, :word_count+1],
                                         latent_seq=latent_code,
                                         src_mask=src_mask,
                                         tgt_mask=tgt_mask,
                                         tgt_lang=lang)
 
             scores = F.softmax(dec_logits, dim=-1)
-            max_score, index = torch.max(scores, -1)
+            max_score, index = torch.max(scores[:, -1], -1)
             print("index", index)
-            prev_output[:, word_count] = index[0, 0].item()
+
+            prev_output[:, word_count] = index.item()
             prev_token = prev_output[:, word_count].item()
-            word = self.data['dico']['en'][index[0, 0].item()]
+            word = self.data['dico'][self.id2lang[lang]][index.item()]
             out.append(word)
             print(out)
     
@@ -77,13 +79,19 @@ class LanguageModeling(Trainer):
 
     def train(self, n_iter):
 
-        opt = torch.optim.Adam(self.transformer.parameters(), lr=0.0001)
+        if cuda.is_available():
+            self.transformer.cuda()
+
+        opt = torch.optim.Adam(self.transformer.parameters(), lr=0.0001,  betas=(0.9, 0.98), eps=1e-9)
         lang = 0
         for i in range(n_iter):
             opt.zero_grad()
             batch, l = self.get_train_batch(lang)
             loss = self.reconstruction_loss(src_batch=batch, lengths=l, lang=lang)
-            print("iter ", i, "loss: ", loss)
+
+            if i % 50 == 0
+                print("iter ", i, "loss: ", loss)
+
             loss.backward()
             opt.step()
 
@@ -103,8 +111,8 @@ if __name__ == "__main__":
     check_all_data_params(data_params)
     model = Transformer(data_params=data_params, embd_file="corpora/mono/all.en-fr.60000.vec")
     trainer = LanguageModeling(model)
-    #trainer.train(200)
-    #trainer.save_model("sanity_check.pth")
+    trainer.train(3000)
+    trainer.save_model("sanity_check.pth")
     trainer.load_model("sanity_check.pth")
     trainer.test(10) 
 
