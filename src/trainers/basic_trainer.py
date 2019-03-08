6e97434ea70c0ac6872fd768766e3b27110879e5
Elsa Riachi <elsa.riachi@mail.mcgill.ca>
2019-03-07 23:05:01 -0500

    Added lm training iterator

--- src/trainers/basic_trainer.py
+++ src/trainers/basic_trainer.py
@@ -8,11 +8,19 @@ from src.data.loader import *
 from abc import ABC, abstractmethod
 
 class Trainer(ABC):
-    
+
     def __init__(self, transformer):
-        
+
         super().__init__()
         self.transformer = transformer
+
+        if torch.cuda.is_available():
+            self.device = torch.device('cuda')
+            self.transformer.cuda()
+
+        else:
+            self.device = torch.device('cpu')
+
         self.data = transformer.data
         self.data_params = transformer.data_params
         self.noise_model = NoiseModel(data=self.data, params=self.data_params)
@@ -21,13 +29,17 @@ class Trainer(ABC):
         self.pad_index = transformer.pad_index
         self.eos_index = transformer.eos_index
         self.bos_index = transformer.bos_index
+        self.id2lang = transformer.id2lang
+
+        get_train_iterators = [self.transformer.train_iterators
+        iterator = get_iterator()
 
     def get_src_mask(self, src_batch):
         mask = torch.ones_like(src_batch)
         mask.masked_fill_(src_batch == self.pad_index, 0).unsqueeze_(-2).unsqueeze_(-2)
         #print("mask", mask)
         return mask
-    
+
     def get_tgt_mask(self, tgt_batch):
 
         batch_size, sent_len = tgt_batch.shape
@@ -42,23 +54,60 @@ class Trainer(ABC):
         tgt_m.masked_fill_(tgt_batch.unsqueeze(-1) == self.pad_index, 0).unsqueeze_(1)
         #print("tgt_m", tgt_m)
         return tgt_m
-    
+
     @abstractmethod
     def train(n_iter):
         pass
 
-    def get_train_batch(self, lang):
+    def get_lm_iterator(self, lang, train=True, add_noise=True):
+        """
+        returns batch with relevant masks
+        moves everything to device
 
-        get_iterator = self.transformer.train_iterators[lang]
-        iterator = get_iterator()
+        :param lang:
+        :param add_noise:
+        :return:
+        """
 
-        batch, l = next(iterator)
-        # print(batch, l)
-        batch = batch.transpose_(0, 1)
-        return batch, l
-    
+        if train:
+            get_src_iterator = self.transformer.train_iterators[lang]
+
+        else:
+            get_src_iterator = self.transformer.val_iterators[lang]
+
+        src_iterator = get_src_iterator()
+
+        def iterator():
+            for tgt_batch, tgt_l in src_iterator:
+                if add_noise:
+                    src_batch, src_l = self.NoiseModel.add_noise(tgt_batch, tgt_l, lang)
+
+                else:
+                    src_batch = tgt_batch
+                    src_l = tgt_l
+
+                src_mask = self.get_src_mask(src_batch)
+                tgt_mask = self.get_tgt_mask(tgt_batch)
+
+                # move to cuda
+                tgt_batch = tgt_batch.to(self.device)
+                src_batch = tgt_batch.to(self.device)
+                src_mask = src_mask.to(self.device)
+                tgt_mask = tgt_mask.to(self.device)
+                src_l = src_l.to(self.device)
+                tgt_l = tgt_l.to(self.device)
+
+                yield {"src_batch": src_batch,
+                       "tgt_batch":tgt_batch,
+                       "src_mask":src_mask,
+                       "tgt_mask": tgt_mask,
+                       "src_l": src_l,
+                       "tgt_l": tgt_l}
+
+        return iterator
+
     def save_model(self, path):
         torch.save(self.transformer.state_dict(), path)
-        
+
     def load_model(self, path):
         self.transformer.load_state_dict(torch.load(path))
--- src/trainers/sanity_check.py
+++ src/trainers/sanity_check.py
@@ -14,15 +14,12 @@ class LanguageModeling(Trainer):
 
         super().__init__(transformer)
 
-    def reconstruction_loss(self, src_batch, lengths, lang, noise=True):
+    def reconstruction_loss(self, batch_dict, lang):
 
-        tgt_mask = self.get_tgt_mask(src_batch)
-        tgt_batch = src_batch.clone()
-        #print(src_batch.shape, lengths)
-        if noise:
-            src_batch, new_len = self.noise_model.add_noise(src_batch, lengths, lang)
-
-        src_mask = self.get_src_mask(src_batch)
+        tgt_mask = batch_dict["tgt_mask"]
+        tgt_batch = batch_dict["tgt_batch"]
+        src_mask = batch_dict["src_mask"]
+        src_batch = batch_dict["src_batch"]
 
         output_seq = self.transformer(input_seq=src_batch,
                                   prev_output=tgt_batch,
@@ -33,8 +30,10 @@ class LanguageModeling(Trainer):
 
         return F.cross_entropy(input=torch.flatten(output_seq, 0, 1),
                                target=torch.flatten(src_batch))
-    
-    def greedy_decoding(self, sent):
+
+    def greedy_decoding(self, sent, src_mask):
+
+        # TODO: check
 
         assert(sent.shape[0] == 1)
         lang = 0
@@ -50,7 +49,7 @@ class LanguageModeling(Trainer):
         word_count = 0
 
         while prev_token is not self.eos_index and word_count<self.max_len-1:
-            
+
             word_count+=1
             tgt_mask = np.ones((1, self.max_len))
             tgt_mask = torch.from_numpy(tgt_mask)
@@ -70,36 +69,29 @@ class LanguageModeling(Trainer):
             word = self.data['dico'][self.id2lang[lang]][index.item()]
             out.append(word)
             print(out)
-    
+
         input = []
         for i in range(sent.size(1)):
             idx = sent[:, i]
             input.append(self.data['dico']['en'][idx])
 
-        print("input ", input)    
+        print("input ", input)
 
     def train(self, n_iter):
 
-        if torch.cuda.is_available():
-            device = torch.device('cuda')
-            self.transformer.cuda()
+        # for param in self.transformer.parameters():
+        #     print(param.get_device())
 
-        else:
-            device = torch.device('cpu')
-        
-        for param in self.transformer.parameters():
-            print(param.get_device())
-        
-        opt = torch.optim.Adam(self.transformer.parameters(), lr=0.0001,  betas=(0.9, 0.98), eps=1e-9)
         lang = 0
+        get_iterator = self.get_lm_iterator(lang=lang, train=True, add_noise=True)
+        train_iterator = get_iterator()
+        opt = torch.optim.Adam(self.transformer.parameters(), lr=0.0001,  betas=(0.9, 0.98), eps=1e-9)
+
         for i in range(n_iter):
             opt.zero_grad()
-            batch, l = self.get_train_batch(lang)
-            batch = batch.to(device)
-            print(batch.type())
-            l =l.to(device)
-            print(l.type())
-            loss = self.reconstruction_loss(src_batch=batch, lengths=l, lang=lang)
+            batch_dict = next_train_iterator()
+
+            loss = self.reconstruction_loss(batch_dict, lang=lang)
 
             if i % 50 == 0:
                 print("iter ", i, "loss: ", loss)
@@ -111,10 +103,11 @@ class LanguageModeling(Trainer):
         self.transformer.eval()
         lang=0
         for i in range(n_tests):
-            batch, l = self.get_train_batch(0)
-            self.greedy_decoding(batch)
+            pass
+            # batch, l = self.get_train_batch(0)
+            # self.greedy_decoding(batch)
             #loss = self.reconstruction_loss(src_batch=batch, lengths=l, lang=lang)
-           
+
 
 if __name__ == "__main__":
 
@@ -126,7 +119,4 @@ if __name__ == "__main__":
     trainer.train(3000)
     trainer.save_model("sanity_check.pth")
     trainer.load_model("sanity_check.pth")
-    trainer.test(10) 
-
-
-
+    trainer.test(10)
