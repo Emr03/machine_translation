fbcdd353f10b7daba6a73d4fb153056ab6a368aa
Elsa Riachi <elsa.riachi@mail.mcgill.ca>
2019-03-11 18:05:00 -0400

    added test for para trainer

--- src/trainers/para_trainer.py
+++ src/trainers/para_trainer.py
@@ -31,6 +31,57 @@ class ParallelTrainer(Trainer):
 
         return self.compute_kl_div_loss(x=output_seq, target=tgt_batch, lang=lang2)
 
+    def greedy_decoding(self, batch_dict, lang):
+
+        tgt_batch = batch_dict["tgt_batch"]
+        src_mask = batch_dict["src_mask"]
+        src_batch = batch_dict["src_batch"]
+
+        if tgt_batch.shape[0] > 1:
+            tgt_batch = tgt_batch[0, :].unsqueeze(0)
+            src_batch = src_batch[0, :].unsqueeze(0)
+            src_mask = src_mask[0, :].unsqueeze(0)
+
+        print(tgt_batch.shape)
+
+        latent_code = self.transformer.module.encode(input_seq=src_batch,
+                                                     src_mask=src_mask,
+                                                     src_lang=lang)
+
+        prev_output = torch.ones(1, self.max_len, dtype=torch.int64) * self.pad_index
+        prev_output = prev_output.to(self.device)
+        out = []
+        prev_output[:, 0] = self.bos_index
+        prev_token = self.bos_index
+        word_count = 0
+
+        while prev_token is not self.eos_index and word_count < self.max_len - 1:
+            word_count += 1
+            tgt_mask = torch.ones(1, word_count + 1).to(self.device)
+            tgt_mask[:, word_count] = 0
+            dec_logits = self.transformer.module.decode(prev_output=prev_output[:, :word_count + 1],
+                                                        latent_seq=latent_code,
+                                                        src_mask=src_mask,
+                                                        tgt_mask=tgt_mask,
+                                                        tgt_lang=lang)
+
+            scores = F.softmax(dec_logits, dim=-1)
+            max_score, index = torch.max(scores[:, -1], -1)
+            # print("index", index)
+
+            prev_output[:, word_count] = index.item()
+            prev_token = prev_output[:, word_count].item()
+            word = self.data['dico'][self.id2lang[lang]][index.item()]
+            out.append(word)
+
+        print("output", out)
+        input = []
+        for i in range(src_batch.size(1)):
+            idx = src_batch[:, i].item()
+            input.append(self.data['dico'][self.id2lang[lang]][idx])
+
+        print("input ", input)
+
     def train(self, n_iter):
 
         # for param in self.transformer.parameters():
@@ -69,6 +120,17 @@ class ParallelTrainer(Trainer):
                 self.logger.debug("Exception in training loop")
                 self.logger.debug(e.message)
 
+    def test(self, n_tests):
+        self.transformer.eval()
+        lang = 0
+        get_iterator = self.get_lm_iterator(lang=lang, train=True, add_noise=True)
+        train_iterator = get_iterator()
+        for i in range(n_tests):
+            batch_dict = next(train_iterator)
+            self.greedy_decoding(batch_dict, lang)
+
+
+
 
 if __name__ == "__main__":
 
@@ -81,7 +143,7 @@ if __name__ == "__main__":
 
     trainer = ParallelTrainer(model)
 
-    trainer.train(30000)
+    trainer.train(3000)
     trainer.save_model("en_fr.pth")
     logger.info("testing trained model")
     trainer.test(10)
